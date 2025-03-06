ed091c1f4a18c2da7c21c5bba1b09a9387532dc2
Sventey <svenluttik@gmail.com>
2025-03-06 18:34:59 +0100

    update

--- Js/index.js
+++ Js/index.js
@@ -6,6 +6,6 @@ primaryHeader.before(scrollWatcher);
 
 const navObserver = new IntersectionObserver((entries) => {
     primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
-}, { rootMargin: "600px 0px 0px 0px" });
+}, { rootMargin: "300px 0px 0px 0px" });
 
 navObserver.observe(scrollWatcher);
